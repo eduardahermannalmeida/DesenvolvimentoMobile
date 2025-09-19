ed81a3e4440b0f92ef2082a1d5f4d2392cf027d4
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2025-09-19 15:09:55 -0300

    Aula 19/09/2025 2ª Edição

--- comprar-aula-02/comprar/src/app/home/index.js
+++ comprar-aula-02/comprar/src/app/home/index.js
@@ -6,9 +6,34 @@ import { FilterStatus } from '../../types/filter-status'
 import { styles } from './styles'
 import { FlatList } from 'react-native'
 import { Item } from '../../components/items'
+import { Alert } from 'react-native'
+
+const FILTER_STATUS = [FilterStatus.PENDING, FilterStatus.DONE]
 
 export function Home() {
 	const [filter, setFilter] = useState(FilterStatus.PENDING)
+	const [description, setDescription] = useState('')
+	const [items, setItems] = useState([])
+
+	async function handleAdd() {
+		if(!description.trim()){
+			return Alert.alert('Adicionar', 'Informe a descrição para adicionar.')
+		}
+
+	const newItem = {
+		id: Math.random().toString().substring(2), // gerando um id aleatório com 2 dígitos
+		description,
+		status: FilterStatus.PENDING,
+	}
+
+	await itemStorage.add(newItem)
+	await itemByStatus()
+
+	Alert.alert('Adicionado', 'Adicionado ${description}')
+	setFilter(FilterByStatus.PENDING)
+	setDescription('')
+
+}
 
 	return (
 		<View style={styles.container}>
