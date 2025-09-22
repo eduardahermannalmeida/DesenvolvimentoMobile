7170a7de6066a55b9573c90ebabdf7f606a7ef9c
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2025-09-22 16:31:07 -0300

    Aula 22/09/2025 3ª Edição

--- boarding-pass-aula-03/boardingpass/src/app/home/index.tsx
+++ boarding-pass-aula-03/boardingpass/src/app/home/index.tsx
@@ -1,4 +1,5 @@
 import { ImageBackground, Text, View } from 'react-native'
+import { Flight } from '@/components/flight'
 import { styles } from './styles'
 
 export function Home() {
@@ -12,7 +13,13 @@ export function Home() {
 				<Text style={styles.subtitle}>Faltam 45 dias para a sua viagem.</Text>
 			</ImageBackground>
 
-			<View style={styles.ticket}></View>
+			<View style={styles.ticket}>
+				<View style={styles.content}>
+					<View style={styles.flight}></View>
+					<Flight label="São Paulo" value="GRU" />
+					<Flight label="Nova York" value="JKF" />
+				</View>
+			</View>
 		</View>
 	)
 }
