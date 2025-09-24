015cb2c963d579ace7e7078ef3f197dec5beb968
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2025-09-24 17:56:45 -0300

    Aula 24/09/2025
    Uso do template navigation(Typescript)

---
+++ links-aula-04/links/src/app/index/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: 42,
+	},
+	header: {
+		paddingHorizontal: 24,
+		width: '100%',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		marginBottom: 32,
+	},
+	logo: {
+		width: 32,
+		height: 32,
+	},
+});
