7e86148182ec76ed1df1d5b6d4174345b634fc76
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2025-09-29 15:18:49 -0300

    Aula 29/09/2025 1ª Edição

--- links-aula-04/links/src/app/index/index.tsx
+++ links-aula-04/links/src/app/index/index.tsx
@@ -2,26 +2,55 @@ import { Categories } from '@/components/categories';
 import { Link } from '@/components/link';
 import { colors } from '@/styles/colors';
 import { MaterialIcons } from '@expo/vector-icons';
-import { Image, TouchableOpacity, View } from 'react-native';
+import {
+	FlatList,
+	Image,
+	Modal,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native';
 import { styles } from './styles';
 
 export default function Index() {
 	return (
 		<View style={styles.container}>
 			<View style={styles.header}>
-				<Image source={require('@/assets/logo.png')} />
+				<Image source={require('@/assets/logo.png')} style={styles.logo} />
 
 				<TouchableOpacity>
-					<MaterialIcons name="add" size={32} colors={colors.green[300]} />
+					<MaterialIcons name="add" size={32} color={colors.green[300]} />
 				</TouchableOpacity>
 			</View>
+
 			<Categories />
 
-			<Link
-				name="Expo"
-				url="https://docs.expo.dev/"
-				onDetails={() => console.log('clicou!')}
+			<FlatList
+				data={['1', '2', '3']}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => (
+					<Link
+						name="Expo"
+						url="https://docs.expo.dev/"
+						onDetails={() => console.log('clicou!')}
+					/>
+				)}
+				style={styles.links}
+				contentContainerStyle={styles.container}
+				showsVerticalScrollIndicator={false}
 			/>
+			<Modal>
+				<View style={styles.modal}>
+					<View style={styles.modalContent}>
+						<View style={styles.modalHeader}>
+							<Text style={styles.modalCategory}>Curso</Text>
+							<MaterialIcons name="close" size={20} color={colors.gray[400]} />
+						</View>
+						<Text style={styles.modalLinkName}>Google</Text>
+						<Text style={styles.modalUrl}>https://google.com</Text>
+					</View>
+				</View>
+			</Modal>
 		</View>
 	);
 }
